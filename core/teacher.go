1fae1a4bb7b1996b111ed5dc56f871f3341c0ddc
t-mamengdi <mmd@abc.com>
2023-04-11 11:54:35 +0800

    “添加教师”功能完成

--- core/teacher.go
+++ core/teacher.go
@@ -8,31 +8,26 @@ import (
 
 func GetTeacher(ctx *gin.Context) {
 	var teachers []Teacher
-	DB.Preload("Class").Find(&teachers)
+	DB.Find(&teachers)
 	ctx.HTML(200, "teacher.html", gin.H{
 		"teachers": teachers,
 	})
 }
 func GetAddTeacherHtml(ctx *gin.Context) {
-	var classes []Class
-	DB.Find(&classes)
-	ctx.HTML(200, "addTeacher.html", gin.H{
-		"classes": classes,
-	})
+	ctx.HTML(200, "addTeacher.html", nil)
 }
 func AddTeacher(ctx *gin.Context) {
 	//获取前端请求数据
-	sno, _ := strconv.Atoi(ctx.PostForm("sno"))
+	tno, _ := strconv.Atoi(ctx.PostForm("tno"))
 	name := ctx.PostForm("name")
 	age, _ := strconv.Atoi(ctx.PostForm("age"))
 	gender := ctx.PostForm("gender")
 	tel := ctx.PostForm("tel")
-	cls, _ := strconv.Atoi(ctx.PostForm("cls"))
 	remark := ctx.PostForm("remark")
 	//赋值给student对象
-	stus := Student{Base: Base{Name: name}, Sno: sno, Age: age, Gender: gender, Tel: tel, ClassID: cls, Remark: remark}
+	teachers := Teacher{Base: Base{Name: name}, Tno: tno, Age: age, Gender: gender, Tel: tel, Remark: remark}
 	//数据库存储
-	DB.Create(&stus)
+	DB.Create(&teachers)
 	//数据库查询
 	ctx.Redirect(301, "/teacher")
 
