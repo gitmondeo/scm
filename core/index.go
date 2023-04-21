018c8fb61ec4fce86a4e90a2c9d065c85978e934
t-mamengdi <mmd@abc.com>
2023-04-21 18:25:06 +0800

    调试访问/异常问题

--- core/index.go
+++ core/index.go
@@ -1,17 +1,25 @@
 package core
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func GetIndex(ctx *gin.Context) {
 	//cookie检验是否登录成功
 	isLogin, _ := ctx.Cookie("isLogin")
-	if isLogin == "true" {
+	fmt.Println("isLogin111::::", isLogin)
+	/*if isLogin == "true" {
 		ctx.HTML(200, "base.html", nil)
 	} else {
+		fmt.Println("isLogin222::::", isLogin)
 		ctx.Redirect(http.StatusMovedPermanently, "/login")
+	}*/
+	if isLogin != "nil" {
+		ctx.HTML(200, "base.html", nil)
+	} else {
+		fmt.Println("hhhhhh")
+		//ctx.Redirect(http.StatusMovedPermanently, "/login")
 	}
 
 	//session判断是否登录成功
